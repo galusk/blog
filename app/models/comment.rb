74ae5cdf25d01dbca7cf6741a5cce573fbb1d507
galusk <kagalus@gmail.com>
2018-03-03 14:54:57 +0100

    validate commenter as email in article

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   validates :body, presence: true, length: { minimum: 6, maximum: 500 }
   #{ within 6..500 } { in 6..500 }
-  validates :commenter, presence: true
+  validates :commenter, presence: true, email: true
 
   belongs_to :article
 end
