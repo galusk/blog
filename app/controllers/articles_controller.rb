4c776600e6debe7b028e4775399690e4d3524d48
galusk <kagalus@gmail.com>
2018-03-04 13:20:41 +0100

    add_tags

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,12 @@ class ArticlesController < ApplicationController
   before_action  :provide_article, only: [:show, :destroy, :edit, :update]
 
   def index
-    @articles = Article.all
+    if params[:q].present?
+      @articles = Article.where("? = any(tags)", params[:q])
+    else
+      @articles = Article.all
+    end
+
   end
 
   def new
